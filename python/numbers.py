9ae74d677fada0da28f8af923f2e5f9e47ede5b3
steve lawrnece <mainasteven328@gmail.com>
2026-02-11 10:54:56 +0300

    programme to add ,subtract, divide , multiply

---
+++ python/numbers.py
@@ -0,0 +1,23 @@
+#name : steve lawrence
+# date:13/2/2026
+
+# program to add , subtract,divide,multiply 2 numbers form input
+
+first_number = int(input("Enter the first number"))
+second_number = int(input("Enter the second number"))
+
+
+sum_numbers = first_number + second_number
+diffence_numbers = first_number - second_number
+quotient_numbers = first_number / second_number
+product_numbers = first_number * second_number
+
+
+
+print(f"The sum of the numbers : {sum_numbers}")
+print(f"The differnce of the numbers : {diffence_numbers}")
+print(f"The quotient of the numbers : {quotient_numbers}")
+print(f"The product of the numbers : {product_numbers}")
+
+
+
---
+++ python/user_input.py
@@ -0,0 +1,10 @@
+#name : steve lawrence
+# date:13/2/2026
+
+# programme to get use input
+
+
+name = input ('what is your name')
+
+print(name)
+
