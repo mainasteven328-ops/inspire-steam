b75ab0fc7b54c16bfb1f0d63cf790e1789d6bcc4
steve lawrnece <mainasteven328@gmail.com>
2026-02-17 10:21:39 +0300

    beak.py,output.py,arithmetic.py

--- python/assingment3.py
+++ python/assingment3.py
@@ -5,27 +5,10 @@
  # sample data as a list of lists
 import math 
 
-print("|______________________________________|")
-print("| angle |  sin  | cos  |  tan  |")
-
-print("|_____________________________________|")
-
-for angle in range (-180,181,30):
-      rad = math.radians(angle)
-      sin_val = round(math.sin(rad), 4)
-      cos_val = round(math.cos(rad), 4)
-
-      if angle == 180 or angle == -180 :
-            tan_val = "undefined"
-            print (f"(angle : {angle:5}) | (sin_val:{sin_val:8.4f}) | (cos_val:{cos_val:8.4f}) | (tan_val:{tan_val:8})")
-            
-            tan_val = round (math.tan(rad),4)
-            print (f"| {angle:5} | {sin_val:8.4f} | {cos_val:8.4f} | {tan_val: 8} |")
-            print("|____________________________________|")
-
-
-
-#print the table with a grid format for  better readability
+print("|__________________________________________________|")
+print(f"| x     |      sin x  |   cos x   |    tan x     | ")
+for x in range ( -180,180,30):
+      print (f" {x} | {math.sin (-180)} |  {math.cos(x)} |  {math.tan(x)} |")
 
 
 
