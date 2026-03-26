d9ab27fb22540670684d4086667747fdbe1b8c7b
steve lawrnece <mainasteven328@gmail.com>
2026-03-26 14:57:09 +0300

    html,css,js intro

---
+++ javascript/javascript/lesson1.js
@@ -0,0 +1,19 @@
+name = "steve lawrence"
+
+age = 24
+weight = 70
+fav_dish = "yoo mum"
+grade = "E"
+
+
+console.log("hello from " + name)
+
+console.log("I am " + age + "years old")
+
+f_number = 45
+s_number = 678
+sum_number = f_number + s_number
+diff_number = f_number - s_number
+product_number = f_number * s_number
+quotient_number = f_number / s_number
+console.log("the sum of the numbers =" + sum_number)
---
+++ javascript/javascript/lesson2.js
@@ -0,0 +1,15 @@
+//this is  a single line comment 
+
+/*this is a
+a multiline comment*/
+age = 24
+
+if (age >=18){
+    console.log("you are allowed to drive")
+}else{
+    console.log("you are not allowed to drive")
+}
+
+while(age<=20){
+    console.log(age)
+}
