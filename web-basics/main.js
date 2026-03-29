a3c0fb83be43dc2e6e29714d3650e31059f14d54
steve lawrnece <mainasteven328@gmail.com>
2026-03-29 17:11:33 +0300

    index.html(button), js

---
+++ web-basics/main.js
@@ -0,0 +1,18 @@
+function message(){
+    alert("hello from king of the jungle osns");
+
+}
+var fnumber,snumber,sumnumber
+function add(fnumber,snumber){
+    sumnumber = fnumber + snumber;
+    return sumnumber;
+}
+
+function changeText()
+{
+     var btn = document.getElementById("mybtn");
+    var span = getElementById("output");
+    var textbox = document.getElementById("textbox");
+    
+    textbox.style.color = "black"
+}
